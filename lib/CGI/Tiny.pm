95a2852e51db7866f7377a16c5b079c144a2c807
Dan Book <grinnz@grinnz.com>
2021-05-02 23:28:36 -0400

    Bump version

--- lib/CGI/Tiny.pm
+++ lib/CGI/Tiny.pm
@@ -7,7 +7,7 @@ use Carp ();
 use IO::Handle ();
 use Exporter ();
 
-our $VERSION = '0.012';
+our $VERSION = '0.013';
 
 use constant DEFAULT_REQUEST_BODY_LIMIT => 16777216;
 use constant DEFAULT_REQUEST_BODY_BUFFER => 262144;
