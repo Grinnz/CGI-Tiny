0180192d0d8f17d581e0aeb902f67a0228177faa
Dan Book <grinnz@grinnz.com>
2021-05-30 17:08:41 -0400

    let's do it

--- lib/CGI/Tiny.pm
+++ lib/CGI/Tiny.pm
@@ -11,7 +11,7 @@ use Carp ();
 use IO::Handle ();
 use Exporter ();
 
-our $VERSION = '0.021';
+our $VERSION = '1.000';
 
 use constant DEFAULT_REQUEST_BODY_LIMIT => 16777216;
 use constant DEFAULT_REQUEST_BODY_BUFFER => 262144;
